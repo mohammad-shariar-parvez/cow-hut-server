19fd13b4b43308887b8e3cfed027d0a98687d257
Piaowei <msptonmoy@gmail.com>
2023-07-03 04:41:14 +0600

    Admin interface added

---
+++ src/app/modules/admin/admin.interface.ts
@@ -0,0 +1,23 @@
+import { Model } from 'mongoose';
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type IAdmin = {
+  id: string;
+  phoneNumber: string;
+  role: 'admin';
+  password: string;
+  name: UserName;
+  address?: string;
+};
+
+export type AdminModel = {
+  isUserExist(id: string): Promise<Pick<IAdmin, 'id' | 'password' | 'role'>>;
+  isPasswordMatched(
+    givenPassword: string,
+    savedPassword: string
+  ): Promise<boolean>;
+} & Model<IAdmin>;
