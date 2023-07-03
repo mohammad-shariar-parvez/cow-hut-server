0dcba8b6e88817836224452ab5623eb1c42b6e6d
Piaowei <msptonmoy@gmail.com>
2023-07-03 20:17:07 +0600

    Spesific user permision in Cow update

--- src/app/modules/cow/cow.controllers.ts
+++ src/app/modules/cow/cow.controllers.ts
@@ -46,6 +46,32 @@ const getSingleCow = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const updateCow = catchAsync(async (req: Request, res: Response) => {
+  const tokenUser = req.user;
+  const id = req.params.id;
+  const updatedData = req.body;
+  const result = await CowService.updateCow(id, updatedData, tokenUser);
+
+  sendResponse<ICow>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Cow updated successfully !',
+    data: result,
+  });
+});
+
+const deleteCow = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await CowService.deleteCow(id);
+
+  sendResponse<ICow>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Cow deleted successfully !',
+    data: result,
+  });
+});
+
 export const CowController = {
   createCow,
   getAllCows,
