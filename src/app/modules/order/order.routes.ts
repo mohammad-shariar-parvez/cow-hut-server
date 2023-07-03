1d02f88b0e87ed257efdbb957a39bb7667cebb95
Piaowei <msptonmoy@gmail.com>
2023-07-03 21:47:13 +0600

    Order routes with authorization middleware

--- src/app/modules/order/order.routes.ts
+++ src/app/modules/order/order.routes.ts
@@ -2,13 +2,22 @@ import express from 'express';
 import { OrderController } from './order.controllers';
 import { requestValidation } from '../../middleware/validationRequest';
 import { OrderValidation } from './order.validation';
+import auth from '../../middleware/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+
 const router = express.Router();
 
 router.post(
   '/order-create',
+  auth(ENUM_USER_ROLE.BUYER),
   requestValidation.validateRequest(OrderValidation.createOrderZodSchema),
   OrderController.createOrders
 );
-router.get('/', OrderController.getAllOrders);
+router.get(
+  '/',
+  auth(ENUM_USER_ROLE.SELLER, ENUM_USER_ROLE.BUYER, ENUM_USER_ROLE.ADMIN),
+  OrderController.getAllOrders
+);
 
+router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), OrderController.getOrder);
 export const OrderRoutes = router;
